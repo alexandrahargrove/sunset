fb20440a1fef065422315d4e15dbe267b6b0a6b5
alexandrahargrove <alexandrahargrove@live.com>
2021-08-20 13:40:48 -0700

    FAQ section

--- src/components/Information.js
+++ src/components/Information.js
@@ -1,6 +1,29 @@
+import React from 'react';
+import Faq from 'react-faq-component';
+
+const faqData = {
+    title: "Frequently Asked Questions",
+    rows: [
+        {
+            title: "Example One Question",
+            content: "Example one answer."
+        },
+        {
+            title: "Example Two Question",
+            content: "Example two answer."
+        },
+        {
+            title: "Example Three Question",
+            content: "Example three answer."
+        }
+    ]
+};
+
 const Information = () => {
     return (
-        <h1>Information here.</h1>
+        <div id="FAQ-Container">
+        <Faq data={faqData} /> 
+        </div>
     )
 }
 
