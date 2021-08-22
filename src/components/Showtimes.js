f60093fb79fd6c0f3297f0344782b87f9db0d1d3
alexandrahargrove <alexandrahargrove@live.com>
2021-08-22 14:44:16 -0700

    Test

--- src/components/Showtimes.js
+++ src/components/Showtimes.js
@@ -2,6 +2,7 @@ const Showtimes = () => {
     return (
         <div id="features">
             <h1>Now Showing</h1>
+            Trying to see if git is working..
             
         </div>
     )
