dbdcdc9d8552f512a9a0776b11ea638e63a167ca
alexandrahargrove <alexandrahargrove@live.com>
2021-08-21 17:46:46 -0700

    Updated FAAQ section.

--- src/components/Information.js
+++ src/components/Information.js
@@ -5,16 +5,36 @@ const faqData = {
     title: "Frequently Asked Questions",
     rows: [
         {
-            title: "Example One Question",
-            content: "Example one answer."
+            title: "Do you accept credit / debit cards?",
+            content: "No. We accept CASH ONLY. However, there is an ATM inside of the snack bar for your convenience."
         },
         {
-            title: "Example Two Question",
-            content: "Example two answer."
+            title: "What is your pet policy?",
+            content: "Pets are ok! We just ask you to keep any furry companions on a leash at all times."
         },
         {
-            title: "Example Three Question",
-            content: "Example three answer."
+            title: "How do I hear the movie?",
+            content: "Tune in to 100.7 AM on your car radio. You can also bring a portable radio if you'd like."
+        },
+        {
+            title: "Can we sit outside our car?",
+            content: "Yes. You can bring foldable chairs or sit in your truck bed."
+        },
+        {
+            title: "How much is it to get in?",
+            content: "$10.00 per adult. $4.00 per childen ages 5-11. 4 and under are free. Cash only."
+        },
+        {
+            title: "When is the Swap Meet?",
+            content: "Every Sunday starting at 5:00am for sellers, and 6:00am for buyers. We will post Saturday evening on Facebook if weather will require us to close. For Swap Meet reservations or questions ONLY please call (805)441-4911."
+        },
+        {
+            title: "Do movies play when it's raining?",
+            content: "Movies play rain or shine."
+        },
+        {
+            title: "When should I arrive?",
+            content: "The Box Office opens at 7:30pm and the first showing starts at 8:15pm."
         }
     ]
 };
